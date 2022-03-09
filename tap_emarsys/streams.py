24d67330c705c788289d4894a7a83fd923fb1219
Gary James <gary.james19@gmail.com>
2022-03-09 10:15:14 +0100

    Update streams.py

--- tap_emarsys/streams.py
+++ tap_emarsys/streams.py
@@ -128,7 +128,7 @@ class ContactFieldsStream(EmarsysStream):
 
     def get_records(self, context: Optional[dict]) -> Iterable[Dict[str, Any]]:
         context = context or {}
-        if context.get('available_fields') is None:
+        if context.get('available_field_ids') is None:
             headers = self.http_headers
             authenticator = self.authenticator
             if authenticator:
@@ -513,31 +513,6 @@ class EmailCampaignTrackedLinksStream(EmarsysStream):
 
 
     def validate_response(self, response: requests.Response) -> None:
-        """Validate HTTP response.
-
-        By default, checks for error status codes (>400) and raises a
-        :class:`singer_sdk.exceptions.FatalAPIError`.
-
-        Tap developers are encouraged to override this method if their APIs use HTTP
-        status codes in non-conventional ways, or if they communicate errors
-        differently (e.g. in the response body).
-
-        .. image:: ../images/200.png
-
-
-        In case an error is deemed transient and can be safely retried, then this
-        method should raise an :class:`singer_sdk.exceptions.RetriableAPIError`.
-
-        Args:
-            response: A `requests.Response`_ object.
-
-        Raises:
-            FatalAPIError: If the request is not retriable.
-            RetriableAPIError: If the request is retriable.
-
-        .. _requests.Response:
-            https://docs.python-requests.org/en/latest/api/#requests.Response
-        """
         if response.status_code == 401:
             msg = (
                 f"{response.status_code} Client Error: "
